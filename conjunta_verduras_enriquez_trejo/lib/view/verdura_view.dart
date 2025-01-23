c78414fce6a4afe7f1ff5706fb4832390ff4621e
Sheylee Arielle Enriquez Hernandez <sheylee.ariel@hotmail.com>
2025-01-23 10:05:49 -0500

    Interfaz

--- conjunta_verduras_enriquez_trejo/lib/view/verdura_view.dart
+++ conjunta_verduras_enriquez_trejo/lib/view/verdura_view.dart
@@ -29,46 +29,96 @@ class _VerduraViewState extends State<VerduraView> {
   Widget build(BuildContext context) {
     final verduraController = Provider.of<VerduraController>(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Gestión de Verduras"),
-      ),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator()) // Muestra un indicador de carga mientras se cargan los datos
-          : verduraController.verduras.isEmpty
-              ? Center(child: Text("No hay verduras disponibles"))
-              : ListView.builder(
-                  itemCount: verduraController.verduras.length,
-                  itemBuilder: (context, index) {
-                    final verdura = verduraController.verduras[index];
-                    return ListTile(
-                      title: Text("${verdura.descripcion}"),
-                      subtitle: Text("Precio: \$${verdura.precio}"),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          IconButton(
-                            icon: Icon(Icons.edit),
-                            onPressed: () {
-                              _showEditDialog(context, verdura);
-                            },
+    return MaterialApp(
+      theme: ThemeData(
+  primarySwatch: Colors.lightBlue,
+  scaffoldBackgroundColor: Color(0xFFE3F2FD),
+  appBarTheme: AppBarTheme(
+    backgroundColor: Color(0xFF81D4FA),
+    titleTextStyle: TextStyle(
+      color: Colors.white,
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+    ),
+    iconTheme: IconThemeData(color: Colors.white),
+  ),
+  floatingActionButtonTheme: FloatingActionButtonThemeData(
+    backgroundColor: Color(0xFF29B6F6),
+  ),
+  textTheme: TextTheme(
+    bodyMedium: TextStyle(fontSize: 16, color: Colors.black87), // Reemplazamos bodyText2 por bodyMedium
+  ),
+),
+
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Gestión de Verduras"),
+        ),
+        body: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : verduraController.verduras.isEmpty
+                ? Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.no_food, size: 50, color: Colors.grey),
+                        SizedBox(height: 10),
+                        Text(
+                          "No hay verduras disponibles",
+                          style: TextStyle(fontSize: 18, color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  )
+                : ListView.builder(
+                    itemCount: verduraController.verduras.length,
+                    itemBuilder: (context, index) {
+                      final verdura = verduraController.verduras[index];
+                      return Card(
+                        margin: EdgeInsets.symmetric(
+                            horizontal: 12, vertical: 8),
+                        color: Color(0xFFB3E5FC),
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundColor: Color(0xFF81D4FA),
+                            child: Icon(
+                              Icons.local_florist,
+                              color: Colors.white,
+                            ),
                           ),
-                          IconButton(
-                            icon: Icon(Icons.delete),
-                            onPressed: () {
-                              verduraController.deleteVerdura(verdura.codigo);
-                            },
+                          title: Text("${verdura.descripcion}",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18)),
+                          subtitle: Text("Precio: \$${verdura.precio}",
+                              style: TextStyle(color: Colors.black54)),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.edit, color: Colors.blue),
+                                onPressed: () {
+                                  _showEditDialog(context, verdura);
+                                },
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.delete, color: Colors.red),
+                                onPressed: () {
+                                  verduraController
+                                      .deleteVerdura(verdura.codigo);
+                                },
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          _showAddDialog(context);
-        },
+                        ),
+                      );
+                    },
+                  ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add, color: Colors.white),
+          onPressed: () {
+            _showAddDialog(context);
+          },
+        ),
       ),
     );
   }
@@ -82,7 +132,8 @@ class _VerduraViewState extends State<VerduraView> {
     showDialog(
       context: context,
       builder: (ctx) => AlertDialog(
-        title: Text("Agregar Verdura"),
+        backgroundColor: Color(0xFFE1F5FE),
+        title: Text("Agregar Verdura", style: TextStyle(color: Colors.black87)),
         content: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -107,7 +158,7 @@ class _VerduraViewState extends State<VerduraView> {
             onPressed: () {
               Navigator.of(ctx).pop();
             },
-            child: Text("Cancelar"),
+            child: Text("Cancelar", style: TextStyle(color: Colors.red)),
           ),
           TextButton(
             onPressed: () {
@@ -119,7 +170,8 @@ class _VerduraViewState extends State<VerduraView> {
               if (codigo <= 0 || descripcion.isEmpty || precio <= 0) {
                 _showErrorDialog(ctx, "Todos los campos son requeridos.");
               } else if (!RegExp(r'^[a-zA-Z\s]+$').hasMatch(descripcion)) {
-                _showErrorDialog(ctx, "La descripción solo debe contener letras.");
+                _showErrorDialog(
+                    ctx, "La descripción solo debe contener letras.");
               } else {
                 Provider.of<VerduraController>(context, listen: false)
                     .addVerdura(Verdura(
@@ -130,7 +182,7 @@ class _VerduraViewState extends State<VerduraView> {
                 Navigator.of(ctx).pop();
               }
             },
-            child: Text("Agregar"),
+            child: Text("Agregar", style: TextStyle(color: Colors.green)),
           ),
         ],
       ),
@@ -139,13 +191,16 @@ class _VerduraViewState extends State<VerduraView> {
 
   // Mostrar el cuadro de diálogo para editar una verdura
   void _showEditDialog(BuildContext context, Verdura verdura) {
-    final descripcionController = TextEditingController(text: verdura.descripcion);
-    final precioController = TextEditingController(text: verdura.precio.toString());
+    final descripcionController =
+        TextEditingController(text: verdura.descripcion);
+    final precioController =
+        TextEditingController(text: verdura.precio.toString());
 
     showDialog(
       context: context,
       builder: (ctx) => AlertDialog(
-        title: Text("Editar Verdura"),
+        backgroundColor: Color(0xFFE1F5FE),
+        title: Text("Editar Verdura", style: TextStyle(color: Colors.black87)),
         content: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -165,7 +220,7 @@ class _VerduraViewState extends State<VerduraView> {
             onPressed: () {
               Navigator.of(ctx).pop();
             },
-            child: Text("Cancelar"),
+            child: Text("Cancelar", style: TextStyle(color: Colors.red)),
           ),
           TextButton(
             onPressed: () {
@@ -176,14 +231,15 @@ class _VerduraViewState extends State<VerduraView> {
               if (descripcion.isEmpty || precio <= 0) {
                 _showErrorDialog(ctx, "Todos los campos son requeridos.");
               } else if (!RegExp(r'^[a-zA-Z\s]+$').hasMatch(descripcion)) {
-                _showErrorDialog(ctx, "La descripción solo debe contener letras.");
+                _showErrorDialog(
+                    ctx, "La descripción solo debe contener letras.");
               } else {
                 Provider.of<VerduraController>(context, listen: false)
                     .updateVerdura(verdura.codigo, descripcion, precio);
                 Navigator.of(ctx).pop();
               }
             },
-            child: Text("Actualizar"),
+            child: Text("Actualizar", style: TextStyle(color: Colors.green)),
           ),
         ],
       ),
@@ -195,8 +251,9 @@ class _VerduraViewState extends State<VerduraView> {
     showDialog(
       context: context,
       builder: (ctx) => AlertDialog(
-        title: Text("Error"),
-        content: Text(message),
+        backgroundColor: Color(0xFFE1F5FE),
+        title: Text("Error", style: TextStyle(color: Colors.red)),
+        content: Text(message, style: TextStyle(color: Colors.black87)),
         actions: [
           TextButton(
             onPressed: () {
